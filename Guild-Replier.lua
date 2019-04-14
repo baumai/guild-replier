719ea76ac5977fb4c0e4c857ec1ce276635a950c
unknown <baumann.maik@burgward.de>
2019-04-14 18:45:31 +0200

    fixed welcome, refactored code

--- Guild-Replier.lua
+++ Guild-Replier.lua
@@ -19,9 +19,16 @@ along with Guild-Replier. If not, see <http://www.gnu.org/licenses/>.
 ]]
 
 local DEBUG_MODE = false;
+
 local timestampLastReplierMessageUnixSeconds = GetServerTime();
 local timestampLastGuildMessageUnixSeconds = GetServerTime();
-local hiSend = false;
+
+function loadedMessage()
+  print("Guild-Replier loaded (made by Berx-Alexstrasza)");
+  if DEBUG_MODE then
+    print("Guild-Replier DEBUG MODE is ON timestampLastReplierMessageUnixSeconds:" .. timestampLastReplierMessageUnixSeconds);
+  end;
+end;
 
 local minimumSecondsBetweenMessagesOrTimer = 60;
 if DEBUG_MODE then
@@ -33,46 +40,27 @@ if DEBUG_MODE then
     secondsCheckForSilence = 10;
 end;
 
+function ownWelcome()
+  sendReplierMessage(getDayTimeBasedGreeting());
+end;
+C_Timer.NewTicker(5, ownWelcome, 1);
+
 function writeSomeGossipWhenItIsSilent()
 	local currentUnixSeconds = GetServerTime();
-  
-      -- spaestens eine minute nach login, besser waere ggf. timer runtersetzen
-	  if not hiSend then
-		  if DEBUG_MODE then
-			SendChatMessage("hi" .. "  debug", "SAY");
-		  else
-			SendChatMessage("hi", "GUILD");
-		  end;
-		  hiSend = true;
-	  end;
 
     if (currentUnixSeconds-secondsCheckForSilence) > timestampLastGuildMessageUnixSeconds then
 		timestampLastGuildMessageUnixSeconds = GetServerTime();
 		
-		local array_words = {"Und sonst so?", "Mit dem Fahrrad ist schneller, als über den Berg ^^", "Hier könnte man noch viele Texte ergänzen..."}
+		local array_words = {
+		  "Und sonst so?", 
+		  "Mit dem Fahrrad ist schneller, als über den Berg ^^", 
+		  "Hier könnte man noch viele Texte ergänzen..."}
 		local answer = array_words[math.random(table.getn(array_words))];
 		
-		if DEBUG_MODE then
-            SendChatMessage(answer .. "  debug", "SAY");
-        else
-            SendChatMessage(answer, "GUILD");
-        end;
+		-- sendReplierMessage(answer);
 	end;
 end;
 C_Timer.NewTicker(minimumSecondsBetweenMessagesOrTimer, writeSomeGossipWhenItIsSilent);
-
-function loadedMessage()
-  print("Guild-Replier loaded (made by Berx-Alexstrasza)");
-  if DEBUG_MODE then
-    print("Guild-Replier DEBUG MODE is ON timestampLastReplierMessageUnixSeconds:" .. timestampLastReplierMessageUnixSeconds);
-  end;
-  
-  if DEBUG_MODE then
-    SendChatMessage("hi" .. "  debug", "SAY");
-  else
-	SendChatMessage("hi", "GUILD");
-  end;
-end;
 
 local frame0=CreateFrame("Frame");
 frame0:RegisterEvent("CHAT_MSG_GUILD");
@@ -94,6 +82,7 @@ frame0:SetScript("OnEvent",function(self,event,msg,byWhomName, arg3,arg4,arg5,ar
         byWhomName, realmName = strsplit("-", byWhomName);
 
         if msg=="hi" or msg=="moin" or msg=="hallo" or msg=="tach" then
+		    --TODO
             -- sagen auch viele anderen gegenueber, ist jetzt online ist da besser
             -- printGuildMessage(byWhomName, "welcome");
         elseif msg=="re" or msg=="wieder da" then
@@ -105,7 +94,7 @@ frame0:SetScript("OnEvent",function(self,event,msg,byWhomName, arg3,arg4,arg5,ar
 
     if event=="CHAT_MSG_SYSTEM" or (DEBUG_MODE and event=="CHAT_MSG_SAY") then
         if DEBUG_MODE then
-            print("second event block msg:"..msg..":");
+            print("second event block msg:" .. msg .. ":");
         end;
 
         if string.find(msg, "ist jetzt online") then
@@ -118,24 +107,27 @@ frame0:SetScript("OnEvent",function(self,event,msg,byWhomName, arg3,arg4,arg5,ar
 
 end);
 
+function getDayTimeBasedGreeting()
+	local numericCurrentHour = tonumber(date("%H", GetServerTime()));
+    if numericCurrentHour > 5 and numericCurrentHour <= 10 then
+		return "moin";
+	elseif numericCurrentHour > 10 and numericCurrentHour < 13 then
+		return "mahlzeit";
+	elseif numericCurrentHour > 17 and numericCurrentHour < 22 then
+		return "nabend";
+	else
+		local array_words = {"hoi", "hey", "tach", "servus", "grüße", "hi", "hallo"}
+		return array_words[math.random(table.getn(array_words))];
+	end;
+end;
+
 function printGuildMessage(byWhom, actionStr)
     if DEBUG_MODE then
         print("printGuildMessage  byWhom:"..byWhom..":  actionStr:"..actionStr..":");
     end;
 
-    local numericCurrentHour = tonumber(date("%H", GetServerTime()));
-
-    local array_words = {"hoi", "hey", "tach", "servus", "grüße", "hi", "hallo"}
-    local answer = array_words[math.random(table.getn(array_words))] .. " " .. byWhom;
-
     if actionStr == "welcome" then
-        if numericCurrentHour > 5 and numericCurrentHour < 10 then
-            answer = "moin " .. byWhom;
-        elseif numericCurrentHour > 10 and numericCurrentHour < 13 then
-            answer = "mahlzeit " .. byWhom;
-        elseif numericCurrentHour > 17 and numericCurrentHour < 22 then
-            answer = "nabend " .. byWhom;
-        end;
+		answer = getDayTimeBasedGreeting() .. " " .. byWhom;
     end;
 
     if actionStr == "returned" then
@@ -156,12 +148,17 @@ function printGuildMessage(byWhom, actionStr)
 
     if (currentUnixSeconds-minimumSecondsBetweenMessagesOrTimer) > timestampLastReplierMessageUnixSeconds then
         timestampLastReplierMessageUnixSeconds = GetServerTime();
-        if DEBUG_MODE then
-            SendChatMessage(answer .. "  debug", "SAY");
-        else
-            SendChatMessage(answer, "GUILD");
-        end;
+        sendReplierMessage(answer);
     elseif DEBUG_MODE then
-        SendChatMessage(answer .. "  debug normally nothing said cause of time", "SAY");
+        sendReplierMessage(answer .. "  debug normally nothing said cause of time");
+    end;
+end;
+
+function sendReplierMessage(text)
+	local txt = "[ " .. text .. " ]";
+	if DEBUG_MODE then
+        SendChatMessage(txt .. "  debug", "SAY");
+    else
+		SendChatMessage(txt, "GUILD");
     end;
 end;
